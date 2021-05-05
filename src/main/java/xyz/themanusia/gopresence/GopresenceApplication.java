123ad52c284bb44fa9b4a94437b762c18371fff9
Ian <minecraftindo0@gmail.com>
2021-05-05 07:27:25 +0700

    Initialize Project

---
+++ src/main/java/xyz/themanusia/gopresence/GopresenceApplication.java
@@ -0,0 +1,13 @@
+package xyz.themanusia.gopresence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GopresenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GopresenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/themanusia/gopresence/GopresenceApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.themanusia.gopresence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GopresenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
